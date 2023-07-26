33fd007d29dc8257201576e2d074cad0f111f0a7
pwnwriter <hey@pwnwriter.xyz>
2023-07-26 15:10:11 +0545

    fix(tests): use library examples to testout

--- examples/buzz.rs
+++ examples/buzz.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello world");
+pub fn max(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_positive_numbers() {
+        assert_eq!(max(2, 3), 3);
+    }
+
+    #[test]
+    fn test_max_negative_numbers() {
+        assert_eq!(max(-5, -7), -5);
+    }
+
+    #[test]
+    fn test_max_positive_and_negative_numbers() {
+        assert_eq!(max(-10, 8), 8);
+    }
 }
