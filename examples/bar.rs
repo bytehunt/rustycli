8567577a7a69722830c406b4ca68c5031714bd5b
pwnwriter <hey@pwnwriter.xyz>
2023-07-26 13:29:08 +0545

    feat(imgs): possible logos, imgs to use

--- examples/bar.rs
+++
@@ -1,7 +0,0 @@
-fn print_message(message: &str) {
-    println!("Message: {}", message);
-}
-
-fn main() {
-    print_messag("Hello, World!"); // Incorrect function name in the call.
-}
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-pub mod interface;
-
-#[tokio::main]
-async fn main() {
-    if let Err(err) = interface::run::run_rustycli().await {
-        eprintln!("Error: {}", err);
-        std::process::exit(1);
-    }
-}
